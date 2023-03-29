880414664037e95349c37335b834cc0ff9cee7d3
Clément <100228798+clementcharmillot@users.noreply.github.com>
2023-03-29 14:57:44 +0200

    Update normal_computation.js

--- GL2_meshes/src/normal_computation.js
+++ GL2_meshes/src/normal_computation.js
@@ -18,6 +18,9 @@ function compute_triangle_normals_and_angle_weights(mesh) {
 		  The absolute value of a float is given by `Math.abs()`.
 	*/
 
+    const compute_tri_area = (a, b, c) =>
+        vec3.length(vec3.cross([0,0,0], vec3.subtract([0,0,0], b, a), vec3.subtract([0,0,0], c, a))) / 2.
+
 	const num_faces     = (mesh.faces.length / 3) | 0
 	const tri_normals   = []
 	const angle_weights = []
@@ -25,10 +28,25 @@ function compute_triangle_normals_and_angle_weights(mesh) {
 		const vert1 = get_vert(mesh, mesh.faces[3*i_face + 0])
 		const vert2 = get_vert(mesh, mesh.faces[3*i_face + 1])
 		const vert3 = get_vert(mesh, mesh.faces[3*i_face + 2])
-		
+
+        // normal = normalize(AB x AC).
+        const normal = vec3.normalize([0,0,0], vec3.cross([0,0,0], vec3.subtract([0,0,0], vert2, vert1), 
+            vec3.subtract([0,0,0], vert3, vert1)));
+
+        // Center = 0.3*A + 0.3B + 0.3C.
+        const tri_center = vec3.add([0,0,0], vec3.add([0,0,0], vert1, vert2), vert3).map(x => x / 3.);
+
+        const tri_area = compute_tri_area(vert1, vert2, vert3);
+
+        const weights = [
+            Math.abs(compute_tri_area(tri_center, vert2, vert3) / tri_area), 
+            Math.abs(compute_tri_area(vert1, tri_center, vert3) / tri_area), 
+            Math.abs(compute_tri_area(vert1, vert2, tri_center) / tri_area), 
+        ];
+
 		// Modify the way triangle normals and angle_weights are computed
-		tri_normals.push([1., 0., 0.])
-		angle_weights.push([1., 1., 1.])
+		tri_normals.push(normal)
+		angle_weights.push(weights)
 	}
 	return [tri_normals, angle_weights]
 }
@@ -43,7 +61,7 @@ function compute_vertex_normals(mesh, tri_normals, angle_weights) {
 
 	const num_faces    = (mesh.faces.length / 3) | 0
 	const num_vertices = (mesh.vertex_positions.length / 3) | 0
-	const vertex_normals = Array(num_vertices).fill([0., 0., 0.])
+	const vertex_normals = Array.from({length: num_vertices}, () => [0., 0., 0.])
 
 	for(let i_face = 0; i_face < num_faces; i_face++) {
 		const iv1 = mesh.faces[3*i_face + 0]
@@ -53,13 +71,16 @@ function compute_vertex_normals(mesh, tri_normals, angle_weights) {
 		const normal = tri_normals[i_face]
 
 		// Add your code for adding the contribution of the current triangle to its vertices' normals
+        const weights = angle_weights[i_face];
 
+        vertex_normals[iv1] = normal.map(x => x * weights[0])
+        vertex_normals[iv2] = normal.map(x => x * weights[1])
+        vertex_normals[iv3] = normal.map(x => x * weights[2])
 	}
 
 	for(let i_vertex = 0; i_vertex < num_vertices; i_vertex++) {
 		// Normalize the vertices
-
-		vertex_normals[i_vertex] = [1., 0., 0.]
+		vertex_normals[i_vertex] = vec3.normalize([0,0,0], vertex_normals[i_vertex])
 	}
 
 	return vertex_normals
